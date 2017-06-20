904ab5b85d106b1001e981e239bcf037f4a76449
zhangxin43 <zhangxin43@meituan.com>
2017-06-20 19:54:27 +0800

    hashMap的hash方法执行流程

--- src/main/java/com/zxahu/base/SymbolTest.java
+++ src/main/java/com/zxahu/base/SymbolTest.java
@@ -11,12 +11,19 @@ Description:
 */
 package com.zxahu.base;
 
+
 public class SymbolTest {
 
 
 
     public static void main(String args[]) {
+        hash();
+    }
 
+    /**
+     * 位移运算
+     */
+    public static void displacement() {
         int count = 3;
         // num << count  num 左移count位,相当于乘2 count次
         int num  = 1 << count;
@@ -30,6 +37,61 @@ public class SymbolTest {
         // 位与, 将2个数字按位与
         num =  511 & count;
         System.out.println(num + " " + Integer.toBinaryString(num));
+    }
+
+    /**
+     * 位运算
+     */
+    private static void bitOp() {
+        int base = 11; // 1011
+        int cal = 5; // 0101
+        // 位与
+        int res = 11 & 5;
+        System.out.println(res + " " + Integer.toBinaryString(res));
+        // 位或
+        res = 11 | 5;
+        System.out.println(res + " " + Integer.toBinaryString(res));
+        // 非
+        res = ~ 11;
+        System.out.println(res + " " + Integer.toBinaryString(res));
+        // 异或
+        res = base ^ cal;
+        System.out.println(res + " " + Integer.toBinaryString(res));
+    }
+
+    /**
+     * HashMap的hash方法执行过程
+     * h ^= (h >>> 20) ^ (h >>> 12);
+     * return h ^ (h >>> 7) ^ (h >>> 4);
+     */
+    private static void hash() {
+        int hashCode = -998662594;
+        System.out.println(hashCode + " " + Integer.toBinaryString(hashCode));
+
+        int step1 = hashCode >>> 20;
+        System.out.println(step1 + " " + Integer.toBinaryString(step1));
+
+        int step2 = hashCode >>> 12;
+        System.out.println(step2 + " " + Integer.toBinaryString(step2));
+
+        int step3 = step1 ^ step2;
+        System.out.println(step3 + " " + Integer.toBinaryString(step3));
+
+        int step4 = hashCode ^ step3;
+        System.out.println(step4 + " " + Integer.toBinaryString(step4));
+
+        int step5 = step4 >>> 7;
+        System.out.println(step5 + " " + Integer.toBinaryString(step5));
+
+        int step6 = step4 >>> 4;
+        System.out.println(step6 + " " + Integer.toBinaryString(step6));
+
+        int res = step4 ^ step5 ^ step6;
+        System.out.println(res + " " + Integer.toBinaryString(res));
+
+        hashCode ^= (hashCode >>> 20) ^ (hashCode >>> 12);
+        res = hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);
+        System.out.println(res + " " + Integer.toBinaryString(res));
     }
 
 
